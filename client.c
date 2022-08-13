9c3170064ee17e2013b9b364ecd7c3c1748f8a68
kshitij-halankar <halanka@uwindsor.ca>
2022-08-13 14:53:48 -0400

    code revamp

--- client.c
+++ client.c
@@ -10,47 +10,52 @@
 #include <sys/errno.h>
 #include <sys/stat.h>
 #include <limits.h>
+#include <libgen.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     char ch;
     int mainpipe_fd, server_fd, client_fd;
     pid_t pid = getpid();
     char *server_main_pipe = "/home/halanka/Desktop/asp/ftp/server/server_main_pipe";
-    
+
     char *server_pipe = malloc(1024 * sizeof(char)); // for receiving data form server
     char *client_pipe = malloc(1024 * sizeof(char)); // for sending commands to server
     strcpy(server_pipe, "/home/halanka/Desktop/asp/ftp/server/serverpipe_");
     strcpy(client_pipe, "/home/halanka/Desktop/asp/ftp/server/clientpipe_");
-    char *client_pid = malloc(6*sizeof(char));
+    char *client_pid = malloc(6 * sizeof(char));
     sprintf(client_pid, "%d", pid);
     strcat(server_pipe, client_pid);
-    strcat(client_pipe,client_pid);
-    printf("server_pipe: %s\n",server_pipe);
-    printf("client_pipe: %s\n",client_pipe);
+    strcat(client_pipe, client_pid);
+    printf("server_pipe: %s\n", server_pipe);
+    printf("client_pipe: %s\n", client_pipe);
 
-	// open main server pipe and write pid
-    while((mainpipe_fd = open(server_main_pipe, O_WRONLY))==-1){
+    // open main server pipe and write pid
+    while ((mainpipe_fd = open(server_main_pipe, O_WRONLY)) == -1)
+    {
         fprintf(stderr, "trying to connect to main pipe\n");
         sleep(5);
     }
     write(mainpipe_fd, &pid, sizeof(pid_t)); // write pid to main server pipe
     close(mainpipe_fd);
-    
 
-    //waiting for server to create command pipes
+    // waiting for server to create command pipes
     sleep(2);
     // sprintf(fifoName,"/home/halanka/Desktop/asp/ftp/server/fifo%ld", pid);
     // sleep(1);
 
-	//open server pipe
-    while((server_fd = open(server_pipe, O_RDONLY))==-1){
+    // open server pipe
+    while ((server_fd = open(server_pipe, O_RDONLY)) == -1)
+    {
         fprintf(stderr, "trying to connect to server pipe %d\n", pid);
         sleep(5);
     }
-    while(1) {
+    while (1)
+    {
         // int i=0;
         // char server_data[1024];
-        while(read(server_fd, &ch, 1) == 1){
+        while (read(server_fd, &ch, 1) == 1)
+        {
             fprintf(stderr, "%c", ch);
             // server_data[i]=ch;
             // i++;
@@ -58,7 +63,8 @@ int main(int argc, char *argv[]){
         close(server_fd);
         chmod(client_pipe, 0777);
 
-        while((client_fd = open(client_pipe, O_WRONLY))==-1){
+        while ((client_fd = open(client_pipe, O_WRONLY)) == -1)
+        {
             fprintf(stderr, "trying to connect to client pipe %d\n", pid);
             sleep(5);
         }
@@ -70,11 +76,13 @@ int main(int argc, char *argv[]){
         write(client_fd, command_buf, strlen(command_buf));
         close(client_fd);
 
-        while((server_fd = open(server_pipe, O_RDONLY))==-1){
+        while ((server_fd = open(server_pipe, O_RDONLY)) == -1)
+        {
             fprintf(stderr, "trying to connect to server pipe %d\n", pid);
             sleep(5);
         }
-        while(read(server_fd, &ch, 1) == 1){
+        while (read(server_fd, &ch, 1) == 1)
+        {
             fprintf(stderr, "%c", ch);
             // server_data[i]=ch;
             // i++;
