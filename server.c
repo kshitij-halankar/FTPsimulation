23660809f62e5a9a2a87218a9dadeab17aba9cb4
kshitij-halankar <halanka@uwindsor.ca>
2022-08-13 20:57:39 -0400

    dele command

--- server.c
+++ server.c
@@ -11,11 +11,12 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <libgen.h>
+#include <dirent.h>
 
 void child(pid_t client);
 void ftp_user();
-void ftp_cwd(char *path);
-void ftp_cdup();
+int ftp_cwd(char *path, char *command_output);
+void ftp_cdup(char *command_output);
 void ftp_rein();
 void ftp_quit();
 void ftp_port(char *pipeName);
@@ -24,10 +25,10 @@ void ftp_stor();
 void ftp_appe();
 void ftp_rest();
 void ftp_rnfr();
-void ftp_rnto();
+void ftp_rnto(char *oldName, char *newname, char *command_output);
 void ftp_abor();
-void ftp_dele(char *fileName, char *result);
-void ftp_rmd();
+void ftp_dele(char *fileName, char *command_output);
+void ftp_rmd(char *dirName, char *command_output);
 void ftp_mkd();
 void ftp_pwd(char *pwd);
 void ftp_list();
@@ -39,7 +40,16 @@ int main(int argc, char *argv[])
     int fd, status;
     char ch;
     pid_t pid;
-    char *server_dir = argv[1];
+    char *server_dir = malloc(PATH_MAX * sizeof(char));
+    if (argc == 2)
+    {
+        strcpy(server_dir, argv[1]);
+        // server_dir = argv[1];
+    }
+    else
+    {
+        getcwd(server_dir, PATH_MAX);
+    }
     char *server_main_pipe = "/home/halanka/Desktop/asp/ftp/server/server_main_pipe";
     // strcat(server_main_pipe, server_dir);
     // strcat(server_main_pipe,"/server_main_pipe");
@@ -76,6 +86,8 @@ int main(int argc, char *argv[])
 
 void child(pid_t pid)
 {
+    int command_counter = 0;
+    int response_code = 0;
 
     char *server_pipe = malloc(1024 * sizeof(char)); // for sending data to client
     char *client_pipe = malloc(1024 * sizeof(char)); // for reading commands from client
@@ -103,6 +115,7 @@ void child(pid_t pid)
     }
 
     write(server_fd, "command pipes created", 22);
+    response_code = 230;
     write(server_fd, &newline, 1);
     close(server_fd);
 
@@ -156,97 +169,105 @@ void child(pid_t pid)
             printf("else: command_name: %s client_command: %s\n", command_name, client_command);
         }
 
-        if (strcmp(command_name, "USER") == 0)
+        // check if USER is first command
+        if (command_counter == 0 && strcmp(command_name, "USER") != 0)
         {
-        }
-        else if (strcmp(command_name, "CWD") == 0)
-        {
-            ftp_cwd(command_parameter);
-            char new_path[PATH_MAX];
-            getcwd(new_path, PATH_MAX);
-            // printf("server directory changed to: %s\n", new_path);
-            command_output = "server directory changed to: ";
-            strcpy(command_output, new_path);
-            printf("%s\n", command_output);
-        }
-        else if (strcmp(command_name, "CDUP") == 0)
-        {
-            ftp_cdup();
-            char new_path[PATH_MAX];
-            getcwd(new_path, PATH_MAX);
-            // printf("server directory changed to: %s\n", new_path);
-            command_output = "server directory changed to: ";
-            strcpy(command_output, new_path);
-            printf("%s\n", command_output);
-        }
-        else if (strcmp(command_name, "REIN") == 0)
-        {
-        }
-        else if (strcmp(command_name, "QUIT") == 0)
-        {
-        }
-        else if (strcmp(command_name, "PORT") == 0)
-        {
-        }
-        else if (strcmp(command_name, "RETR") == 0)
-        {
-        }
-        else if (strcmp(command_name, "STOR") == 0)
-        {
-        }
-        else if (strcmp(command_name, "APPE") == 0)
-        {
-        }
-        else if (strcmp(command_name, "REST") == 0)
-        {
-        }
-        else if (strcmp(command_name, "RNFR") == 0)
-        {
-        }
-        else if (strcmp(command_name, "RNTO") == 0)
-        {
-        }
-        else if (strcmp(command_name, "ABOR") == 0)
-        {
-        }
-        else if (strcmp(command_name, "DELE") == 0)
-        {
-        }
-        else if (strcmp(command_name, "RMD") == 0)
-        {
-        }
-        else if (strcmp(command_name, "MKD") == 0)
-        {
-        }
-        else if (strcmp(command_name, "PWD") == 0)
-        {
-            char path[PATH_MAX];
-            ftp_pwd(path);
-            // printf("current path: %s\n", path);
-
-            strcpy(command_output, "current path: ");
-            strcpy(command_output, path);
-            printf("%s\n", command_output);
-        }
-        else if (strcmp(command_name, "LIST") == 0)
-        {
-        }
-        else if (strcmp(command_name, "STAT") == 0)
-        {
-        }
-        else if (strcmp(command_name, "NOOP") == 0)
-        {
-            char ok[10];
-            ftp_noop(ok);
-
-            strcpy(command_output, ok);
-            printf("%s\n", command_output);
+            strcpy(command_output, "Please run USER command to login and establish connection.");
         }
         else
         {
-            // invalid command
-            strcpy(command_output, "invalid command: ");
-            strcat(command_output, command_name);
+            if (strcmp(command_name, "USER") == 0)
+            {
+                if (response_code == 230)
+                {
+                    strcpy(command_output, "230 User logged in, proceed.");
+                }
+                else
+                {
+                    strcpy(command_output, "530 Not logged in.");
+                }
+            }
+            else if (strcmp(command_name, "CWD") == 0)
+            {
+                printf("param length: %d\n", strlen(command_parameter));
+                ftp_cwd(command_parameter, command_output);
+                printf("%s\n", command_output);
+            }
+            else if (strcmp(command_name, "CDUP") == 0)
+            {
+                ftp_cdup(command_output);
+                // printf("%s\n", command_output);
+            }
+            else if (strcmp(command_name, "REIN") == 0)
+            {
+            }
+            else if (strcmp(command_name, "QUIT") == 0)
+            {
+            }
+            else if (strcmp(command_name, "PORT") == 0)
+            {
+            }
+            else if (strcmp(command_name, "RETR") == 0)
+            {
+            }
+            else if (strcmp(command_name, "STOR") == 0)
+            {
+            }
+            else if (strcmp(command_name, "APPE") == 0)
+            {
+            }
+            else if (strcmp(command_name, "REST") == 0)
+            {
+            }
+            else if (strcmp(command_name, "RNFR") == 0)
+            {
+            }
+            else if (strcmp(command_name, "RNTO") == 0)
+            {
+            }
+            else if (strcmp(command_name, "ABOR") == 0)
+            {
+            }
+            else if (strcmp(command_name, "DELE") == 0)
+            {
+                ftp_dele(command_parameter, command_output);
+            }
+            else if (strcmp(command_name, "RMD") == 0)
+            {
+            }
+            else if (strcmp(command_name, "MKD") == 0)
+            {
+            }
+            else if (strcmp(command_name, "PWD") == 0)
+            {
+                char path[PATH_MAX];
+                ftp_pwd(path);
+                // printf("current path: %s\n", path);
+
+                strcpy(command_output, "current path: ");
+                strcpy(command_output, path);
+                printf("%s\n", command_output);
+            }
+            else if (strcmp(command_name, "LIST") == 0)
+            {
+            }
+            else if (strcmp(command_name, "STAT") == 0)
+            {
+            }
+            else if (strcmp(command_name, "NOOP") == 0)
+            {
+                char ok[10];
+                ftp_noop(ok);
+
+                strcpy(command_output, ok);
+                printf("%s\n", command_output);
+            }
+            else
+            {
+                // invalid command
+                strcpy(command_output, "invalid command: ");
+                strcat(command_output, command_name);
+            }
         }
 
         while ((server_fd = open(server_pipe, O_WRONLY)) == -1)
@@ -254,30 +275,40 @@ void child(pid_t pid)
             fprintf(stderr, "trying to connect to server pipe %d\n", pid);
             sleep(5);
         }
-
+        printf("command_output: %s\n", command_output);
         write(server_fd, command_output, strlen(command_output));
         write(server_fd, &newline, 1);
         close(server_fd);
+        command_counter++;
     }
-
-    // fd = open(server_pipe, O_WRONLY);
-    // for(i=0; i < 10; i++){
-    // write(fd, fifoName, strlen(fifoName));
-    // write(fd, &newline, 1);
-    // }
-    // close(fd);
-    // unlink(fifoName);
-    // exit(0);
 }
 
-void ftp_cwd(char *path)
+int ftp_cwd(char *path, char *command_output)
 {
-    chdir(path);
+    DIR *dir_path = opendir(path);
+    if (dir_path)
+    {
+        chdir(path);
+        closedir(dir_path);
+        strcpy(command_output, "server directory changed to: ");
+        char *new_path = malloc(255 * sizeof(char));
+        getcwd(new_path, 255);
+        strcat(command_output, new_path);
+        // printf("command_output: %s\n", command_output);
+    }
+    else
+    {
+        strcpy(command_output, "failed to change directory.");
+    }
 }
 
-void ftp_cdup()
+void ftp_cdup(char *command_output)
 {
     chdir("..");
+    strcpy(command_output, "server directory changed to: ");
+    char *new_path = malloc(255 * sizeof(char));
+    getcwd(new_path, 255);
+    strcat(command_output, new_path);
 }
 
 void ftp_port(char *pipeName)
@@ -296,17 +327,48 @@ void ftp_noop(char *ok)
     strcpy(ok, "OK");
 }
 
-void ftp_dele(char *fileName, char *result)
+void ftp_dele(char *fileName, char *command_output)
 {
     char *input_file = strdup(fileName);
     char *fname = basename(input_file);
-    printf("filename: %s \n", fname);
+    // printf("filename: %s \n", fname);
     if (remove(fname) == 0)
     {
-        strcpy(result, "Deleted successfully");
+        sprintf(command_output, "File %s deleted successfully", fname);
     }
     else
     {
-        strcpy(result, "Unable to delete the file");
+        sprintf(command_output, "Unable to delete the file %s", fname);
+    }
+}
+
+void ftp_rmd(char *dirName, char *command_output)
+{
+    char *dirname = dirName;
+    int ret = rmdir(dirName);
+    if (ret == 0)
+    {
+        sprintf(command_output, "Directory %s removed successfully", dirName);
+    }
+    else
+    {
+        sprintf(command_output, "Unable to remove directory %s", dirName);
+    }
+}
+
+void ftp_rnto(char *oldName, char *newname, char *command_output)
+{
+    char *ts1 = strdup(oldName);
+    char *old_name = basename(ts1);
+    char *ts2 = strdup(newname);
+    char *new_name = basename(ts2);
+    int result = rename(old_name, new_name);
+    if (result == 0)
+    {
+        sprintf(command_output, "The file %s is renamed successfully to %s", oldName, newname);
+    }
+    else
+    {
+        sprintf(command_output, "The file %s could not be renamed to %s", oldName, newname);
     }
 }
